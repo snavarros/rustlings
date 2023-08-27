4808deedac1e564522fafee7eb59d044882beef9
Sebastian Navarro <sebastian.nav@live.cl>
2023-08-26 21:21:02 -0400

    move_semantics6

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple:u16) -> u16 { 
+    let mut cost = 2;
+    if apple > 40 {
+        let cost = 1;
+        return apple * cost;
+    }
+    return apple * cost;
+}
 
 // Don't modify this function!
 #[test]
